522ee90d177633baae976c51cc10d9b8b9b6c66f
Marc Granier <mgranier@comcast.net>
2024-11-24 14:08:58 -0500

    Complete Image Input

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -4,11 +4,24 @@ import { ZodSchema } from 'zod';
 export const profileSchema = z.object({
   firstName: z
     .string()
-    .min(2, { message: 'first name mst ne at least 2 characters' }),
+    .min(2, { message: 'First name must be at least 2 characters' }),
   lastName: z
     .string()
-    .min(2, { message: 'last name must be at least 2 characters' }),
+    .min(2, { message: 'Last name must be at least 2 characters' }),
   username: z
     .string()
-    .min(2, { message: 'username must be at least 2 characters' }),
+    .min(2, { message: 'Username must be at least 2 characters' }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+    throw new Error(errors.join(','));
+  }
+  return result.data;
+}
